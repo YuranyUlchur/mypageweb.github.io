e6ac4bd9217e212a3771d6e15579122b1b96e142
YuranyUlchur <ulchuryurani80@gmail.com>
2023-08-17 23:07:01 -0500

    add changes in styles in code

--- components/HeaderSection/HeaderSection.js
+++ components/HeaderSection/HeaderSection.js
@@ -4,10 +4,13 @@ import styles from './header.module.css';
 const HeaderSection = () => {
     return (
         <header className={styles.masthead}>
-            <div className={styles.containerheading}>
-                <h1 className={styles.mastheadHeading}>Bienvenid@ a mi Portafolio</h1>
-                <p className={styles.titleprofession}>Desarrolladora de software</p>
+            <div className={styles.border-box}>
+                <div className={styles.containerheading}>
+                    <h1 className={styles.mastheadHeading}>Bienvenid@ a mi Portafolio</h1>
+                    <p className={styles.titleprofession}>Desarrolladora de software</p>
+                </div>
             </div>
+
         </header>
     );
 };
