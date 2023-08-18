65ce3adea9e00edd2416d4619ce6668f3bfd2891
YuranyUlchur <ulchuryurani80@gmail.com>
2023-08-17 23:05:13 -0500

    add changes in styles in code

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -14,11 +14,11 @@ const Footer = () => {
                         <p className={styles.plocation}>Cali - Colombia</p>
                     </div>
                     <div className={styles.dividerCustom}>
-                            <div className={styles.dividerCustomLine}></div>
-                            <div className={styles.dividerCustomIcon}>
-                                <AiOutlineHeart />
-                            </div>
-                            <div className={styles.dividerCustomLine}></div>
+                        <div className={styles.dividerCustomLine}></div>
+                        <div className={styles.dividerCustomIcon}>
+                            <AiOutlineHeart />
+                        </div>
+                        <div className={styles.dividerCustomLine}></div>
                     </div>
                     <div>
                         <h4 className={styles.titlecontact}>Contacto</h4>
