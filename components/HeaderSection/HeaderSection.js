77843a0108c1c8c69f65624cf8018ce4ab355636
YuranyUlchur <ulchuryurani80@gmail.com>
2023-08-17 21:11:54 -0500

    add styles in text for header

--- components/HeaderSection/HeaderSection.js
+++ components/HeaderSection/HeaderSection.js
@@ -7,15 +7,8 @@ const HeaderSection = () => {
         <header className={styles.masthead}>
             <div>
                 <img className={styles.mastheadAvatar} src="/img/fotoperfil.jpeg"/>
-                <h1 className={styles.mastheadHeading}>Bienvenid@ a mi Portafolio</h1>
-                <div className={styles.dividerCustom}>
-                    <div className={styles.dividerCustomLine}></div>
-                    <div className={styles.dividerCustomIcon}>
-                        <AiOutlineHeart />
-                    </div>
-                    <div className={styles.dividerCustomLine}></div>
-                </div>
-                <p className={styles.titleprofession}>Desarrolladora de software</p>
+                <h2 className={styles.mastheadHeading}>Bienvenid@ a mi portafolio personal</h2>
+                <p className={styles.titleprofession}>My name is Yurany ulchur and I am a passionate and creative software developer, eager to learn and find new challenges.</p>
             </div>
         </header>
     );
